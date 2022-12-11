e7c7575e13c4d86683bdec9f4edf4f054fea9b51
Caitlin Cradick <caitlincradick@gmail.com>
2022-12-11 12:15:03 -0800

    add challenge

--- objects.js
+++ objects.js
@@ -33,22 +33,22 @@
 // Declare a variable that stores an object. Each key should be an attribute of an email and each
 // value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
 
-var emailComponents = {
-  sender: "turing@turing.edu",
-  subject: "FE",
-  body: "Welcome to Turing",
-  timeStamp: "12:00 pm - 11 Dec 2022", 
-  signature: "Turing"
-};
+// var emailComponents = {
+//   sender: "turing@turing.edu",
+//   subject: "FE",
+//   body: "Welcome to Turing",
+//   timeStamp: "12:00 pm - 11 Dec 2022", 
+//   signature: "Turing"
+// };
 
-Object(emailComponents); 
-console.log(emailComponents);
+// Object(emailComponents); 
+// console.log(emailComponents);
 
-Object.keys(emailComponents);
-console.log (Object.keys(emailComponents));
+// Object.keys(emailComponents);
+// console.log (Object.keys(emailComponents));
 
-Object.values(emailComponents); 
-console.log(Object.values(emailComponents));
+// Object.values(emailComponents); 
+// console.log(Object.values(emailComponents));
 
 // #-------------------
 // // Part 3: Many Emails - CHALLENGE!
@@ -68,25 +68,25 @@ console.log(Object.values(emailComponents));
 // that relate to each individual Instagram post:
 
 
-// posts = [
-//   {
-//     imageSrc: "./images/beach.png",
-//     caption: "At the beach with my besties",
-//     timestamp: "4:37 PM August 13, 2019",
-//     number_likes: 0,
-//     comments: []
-//   }, 
-//   {
-//     imageSrc: "./images/holiday-party.png",
-//     caption: "What a great holiday party omg",
-//     timestamp: "11:37 PM December 31, 2019",
-//     number_likes: 13,
-//     comments: []
-//   }
-// ];
+posts = [
+  {
+    imageSrc: "./images/beach.png",
+    caption: "At the beach with my besties",
+    timestamp: "4:37 PM August 13, 2019",
+    number_likes: 0,
+    comments: []
+  }, 
+  {
+    imageSrc: "./images/holiday-party.png",
+    caption: "What a great holiday party omg",
+    timestamp: "11:37 PM December 31, 2019",
+    number_likes: 13,
+    comments: []
+  }
+];
 
-// console.log(posts);
-// console.log(posts[0]);
+console.log(posts);
+console.log(posts[0]);
 
 
 // The code snippet above shows an Array with 2 elements. Each element in the Array is a 
@@ -98,3 +98,23 @@ console.log(Object.values(emailComponents));
 // YOU DO: Create an array of at least 3 EMAIL Objects, using the same 
 // key-value pairs you used in your email Object above.
 // Then, print the email Array to the Terminal.
+
+emailComponents = [
+  {
+    sender: "turing@turing.edu",
+    subject: "FE",
+    body: "Welcome to Turing",
+  },
+  {
+  sender: "caitlin@gmail.com",
+  subject: "hey girl, hey",
+  body: "Go on a walk"
+ },
+ {
+sender: "april@gmail,com",
+subject: "Climbing",
+body: "Let's go to the climbing gym"
+ }
+];
+console.log(emailComponents);
+console.log(emailComponents[0]);
