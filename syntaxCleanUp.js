b80eba8e5de4c48b36f63c2e6090dc8166bc1715
Caitlin Cradick <caitlincradick@gmail.com>
2022-12-14 18:29:08 -0800

    add var post after var post2 to reflect changes

--- syntaxCleanUp.js
+++ syntaxCleanUp.js
@@ -9,18 +9,18 @@ var person = {
   weight: "160 lbs" 
 };
 
-var post = { 
-  imageSrc: "./images/beach.png",
-  caption: "At the beach with my besties", 
-  timestamp: "4:37 PM August 13, 2019",
-  number_likes: 0,
-comments: [] 
-};
-
 var post2 = {
   imageSrc: "./images/holiday-party.png",
   caption: "What a great holiday party omg",
   timestamp: "11:37 PM December 31, 2019",
   numberLikes: 13,
   comments: []
+};
+
+var post = { 
+  imageSrc: "./images/beach.png",
+  caption: "At the beach with my besties", 
+  timestamp: "4:37 PM August 13, 2019",
+  number_likes: 0,
+comments: [] 
 };
